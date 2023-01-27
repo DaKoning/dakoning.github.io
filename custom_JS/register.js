0b5cda92fca26b529eb2200e7f2ac1c6862f5773
DaKoning <daaf01@live.nl>
2023-01-27 14:35:44 +0100

    tsa

--- custom_JS/register.js
+++ custom_JS/register.js
@@ -132,18 +132,118 @@ function noError(field, error) {
     error.innerHTML = "Looks good!";
 }
 
+// Functions to check string for certain characters, as we can't use regex
+
+function containsLowercase(string) {
+    for (let i = 0; i < string.length; i++) {
+        if (string.charAt(i) >= 'a' && string.charAt(i) <= 'z') return true;
+    }
+    return false;
+}
+
+function containsUppercase(string) {
+    for (let i = 0; i < string.length; i++) {
+        if (string.charAt(i) >= 'A' && string.charAt(i) <= 'Z') return true;
+    }
+    return false;
+}
+
+function containsNumber(string) {
+    for (let i = 0; i < 10; i++) {
+        if (string.includes(i)) return true;
+    }
+    return false;
+}
+
+function containsSpecialChar(string) {
+    let special = "~`!@#$%^&*()_-+={[}]|\\:;\"\'<,>.?/";
+    for (let i = 0; i < special.length; i++) {
+        if (string.includes(special.charAt(i))) return true;
+    }
+    return false;
+}
+
+function onlyAlphabet(string) {
+    let ch;
+    for (let i = 0; i < string.length; i++) {
+        ch = string.charAt(i);
+        if (!(
+            (ch >= 'a' && ch <= 'z') ||
+            (ch >= 'A' && ch <= 'Z') ||
+            (ch == " ")
+        )) return false;
+    }
+    return true;
+}
+
+function onlyNumbers(string) {
+    let ch;
+    for (let i = 0; i < string.length; i ++) {
+        ch = string.charAt(i);
+        if (!(ch >= "0" && ch <= "9")) return false;
+    }
+    return true;
+}
+
+function goodEmail(string) {
+    // /[A-Za-z0-9._+-]+@[A-Za-z-]+\.[A-Za-z0-9.-]{2,}/
+
+    let arr = string.split('@');
+    if (arr.length-1 != 1) return false; // if there are no @'s
+    if (arr[0].length < 1) return false;
+
+    let ch;
+    for (let i = 0; i < arr[0].length; i++) {
+        ch = arr[0].charAt(i);
+        if (!(
+            (ch >= 'a' && ch <= 'z') ||
+            (ch >= 'A' && ch <= 'Z') ||
+            (ch >= "0" && ch <= "9") ||
+            ch == "." || ch == "_" || ch == "+" || ch == "-"
+        )) return false;
+    }
+
+    let arr2 = arr[1].split(".");
+    if (arr2.length-1 < 1) return false; // if there are no .'s
+    for (let i = 0; i < arr2[0].length; i++) {
+        ch = arr2[0].charAt(i);
+        if (!(
+            (ch >= 'a' && ch <= 'z') ||
+            (ch >= 'A' && ch <= 'Z') ||
+            ch == "-"
+        )) return false;
+    }
+
+    
+
+    for (let i = 0; i < arr2[1].length; i++) {
+        ch = arr2[1].charAt(i);
+        if (!(
+            (ch >= 'a' && ch <= 'z') ||
+            (ch >= 'A' && ch <= 'Z') ||
+            (ch >= "0" && ch <= "9") ||
+            ch == "." || ch == "-"
+        )) return false;
+    }
+    if (arr2[1].length < 2) return false;
+
+    return true;
+}
+
+
 function checkUsername() {
     if (username.value.length < 5 || username.value.length > 12) {
         hasError(username, unameError);
         unameError.innerHTML = "Username must be between 5 and 12 characters long.";
         return false;
     }
-    else if (!/[A-Z]/.test(username.value.charAt(0))) {
+    else if (!containsUppercase(username.value.charAt(0))) {
         hasError(username, unameError);
         unameError.innerHTML = "Username must start with an uppercase letter.";
         return false;
     }
-    else if (!(/\d|\W/.test(username.value.charAt(username.value.length-1)))) {
+    else if (!(containsNumber(username.value.charAt(username.value.length-1)) 
+                || containsSpecialChar(username.value.charAt(username.value.length-1)))) {
         hasError(username, unameError);
         unameError.innerHTML = "Username must end with a digit or a special character.";
         return false;
@@ -160,8 +260,8 @@ function checkPassword() {
         passwordError.innerHTML = "Password must be at least 12 characters long, but 14 or more is better.";
         return false;
     }
-    else if (!(/[A-Z]/.test(password.value) && /[a-z]/.test(password.value)
-                && /\d/.test(password.value) && /\W/.test(password.value))) {
+    else if (!(containsLowercase(password.value) && containsUppercase(password.value)
+                && containsNumber(password.value) && containsSpecialChar(password.value))) {
         hasError(password, passwordError);
         passwordError.innerHTML = "Password must contain all of the following: uppercase letters, lowercase letters, numbers, symbols.";
         return false;
@@ -180,7 +280,7 @@ function checkPassword() {
 }
 
 function checkName(name) {
-    if (/[^A-Za-z\s]/.test(name)) return false;
+    if (!onlyAlphabet(name)) return false;
     return true;
 }
 
@@ -219,7 +319,7 @@ function checkLname() {
 }
 
 function checkStreet() {
-    if (/[^A-Za-z\s]/.test(street.value)) {
+    if (!onlyAlphabet(street.value)) {
         hasError(street, streetError);
         streetError.innerHTML = "Street can only contain letters from the alphabet.";
         return false;
@@ -231,7 +331,7 @@ function checkStreet() {
 }
 
 function checkNumber() {
-    if (/\D/.test(number.value)) {
+    if (!onlyNumbers(number.value)) {
         hasError(number, numberError);
         numberError.innerHTML = "House number must be a number.";
         return false;
@@ -243,7 +343,11 @@ function checkNumber() {
 }
 
 function checkZipcode() {
-    if (!(/\d\d\d\d[a-z][a-z]/i.test(zipcode.value)) || zipcode.value.length != 6) {
+    if (!(
+        onlyNumbers(zipcode.value.substring(0, 4)) &&
+        onlyAlphabet(zipcode.value.substring(4, 6)) &&
+        zipcode.value.length == 6
+    )) {
         hasError(zipcode, zipcodeError);
         zipcodeError.innerHTML = "Zipcode must be of form 1234AB.";
         return false;
@@ -267,7 +371,7 @@ function checkCountry() {
 }
 
 function checkEmail() {
-    if (!(/[A-Za-z0-9._+-]+@[A-Za-z-]+\.[A-Za-z0-9.-]{2,}/.test(email.value))) {
+    if (!goodEmail(email.value)) {
         hasError(email, emailError);
         emailError.innerHTML = "Email not valid.";
         return false;
